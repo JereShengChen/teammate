5ac99f49c21ef57d5e831dd9453eb945a1f07777
shengchen <shengchen2014@gmail.com>
2023-05-30 22:00:30 +1200

    Initial commit

---
+++ teammate.js
@@ -0,0 +1,23 @@
+let firstName = [
+  "Dorie",
+  "Stack",
+  "Coder",
+  "Emergency",
+  "Test",
+];
+
+let lastName = [
+  "404",
+  "Warriors",
+  "Squad",
+  "Cooper&Co",
+  "Group",
+];
+
+function generateName() {
+  let randomFirstName = firstName[Math.floor(Math.random() * firstName.length)];
+  let randomLastName = lastName[Math.floor(Math.random() * lastName.length)];
+
+  const generatedName = `${randomFirstName} ${randomLastName}`;
+  document.getElementById("generatedName").textContent = generatedName;
+}
